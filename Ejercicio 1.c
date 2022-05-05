15e7f93d572e767cf93abea6a193b89db0cc97e3
Daniel Coronado <99162029+DanielCoronadoM@users.noreply.github.com>
2022-05-05 14:13:39 -0500

    Update code

--- Ejercicio 1.c
+++ Ejercicio 1.c
@@ -7,18 +7,18 @@ int x;
 int8 aux_1;
 int8 aux_2;
 
-   set_tris_c(0xFF);
-   set_tris_b(0x00);
+set_tris_c(0xFF);
+set_tris_b(0x00);
    
    while(true){
-   if (input(pin_c0)==1){
-      aux_1=1;
-      for(x=1;x<=8;x++){
-         output_d(aux_1);
-         delay_ms(500);
-         aux_1 = aux_1*2;
-       }
-   }
+      if (input(pin_c0)==1){
+         aux_1=1;
+         for(x=1;x<=8;x++){
+            output_d(aux_1);
+            delay_ms(500);
+            aux_1 = aux_1*2;
+         }
+      }
    
    if(input(pin_c1)==1){
        aux_2=128;
@@ -26,8 +26,7 @@ int8 aux_2;
          output_d(aux_2);
          delay_ms(500);
          aux_2 = aux_2/2;
-       }
+      }
    }    
  }     
 }
-
